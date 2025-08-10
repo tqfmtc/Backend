a278707385fbf67c10d5c6ae2cbb7a21b382f26f
codingsenpy <mustafakunain06@gmail.com>
2025-08-10 13:56:55 +0530

    host port fix

--- .history/server_20250810135620.js
+++ .history/server_20250810135620.js
@@ -53,6 +53,6 @@ app.use((err, req, res, next) => {
 
 const PORT = 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
