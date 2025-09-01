5d90ca6acd4f6cb62ac505c018a5f9229c1a64d5
codingsenpy <mustafakunain06@gmail.com>
2025-09-01 23:23:31 +0530

    bug fix2

--- .history/models/Tutor_20250901231946.js
+++ .history/models/Tutor_20250901231946.js
@@ -48,7 +48,7 @@ const tutorSchema = mongoose.Schema(
     },
     qualificationStatus: {
       type: String,
-      enum: ['','pursuing', 'completed'],
+      enum: ['pursuing', 'completed'],
       default: ''
     },
     yearOfCompletion: {
